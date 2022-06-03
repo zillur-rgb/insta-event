34fb7c7cf07c9dee18a754ca25b9494122255f8c
zillur-rgb <zillurdeu@gmail.com>
2022-06-03 19:00:07 +0200

    project setup and installation

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        main: "#12B0E8",
+        text: "#242B2E",
+      },
+    },
+  },
+  plugins: [],
+};
