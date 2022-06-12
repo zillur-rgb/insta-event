d99e547478077522a08ff7cf92a161bb1a5f65f8
zillur-rgb <zillurdeu@gmail.com>
2022-06-12 10:09:28 +0200

    typescript file setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import Navbar from "./Components/Navbar";
-import AllPages from "./Components/Pages/AllPages";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div className=" w-screen h-screen">
-      <Navbar />
-      <AllPages />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
